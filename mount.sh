206ac962c323bd031ee1aa533b79b7bcc60ced91
Kyle Harding <kmharding@pm.me>
2020-05-18 08:02:21 -0400

    change ownership of storage mount to www-data

--- mount.sh
+++ mount.sh
@@ -7,10 +7,12 @@ do
     chown -v www-data:www-data /media/"${uuid}"
 done
 
+MOUNT_OPTS="uid=www-data,gid=www-data,${EXTRA_MOUNT_OPTS}"
+
 if [ -n "${EXTRA_MOUNT}" ]
 then
     mkdir -v /mnt/storage 2>/dev/null
-    mount -v ${EXTRA_MOUNT} /mnt/storage
+    mount -v ${EXTRA_MOUNT} -o ${MOUNT_OPTS} /mnt/storage
 fi
 
 exit 0
