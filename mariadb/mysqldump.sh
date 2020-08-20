a4cdb1a4e778ab6349ff2edec1059bbb08da6a9f
Kyle Harding <kmharding@pm.me>
2020-08-20 07:40:25 -0400

    make mysqldump script a bit more generic

--- mariadb/mysqldump.sh
+++ mariadb/mysqldump.sh
@@ -5,5 +5,9 @@ set -eu
 while :
 do
     sleep 1h
-    mysqldump --single-transaction -h mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" nextcloud > /var/lib/mysql/nextcloud-mysqldump.sql
+    mysqldump --single-transaction \
+        -h mariadb \
+        -u root \
+        -p"${MYSQL_ROOT_PASSWORD}" \
+        "${MYSQL_DATABASE}" > "/var/lib/mysql/${MYSQL_DATABASE}.sqldump"
 done
