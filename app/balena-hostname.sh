84905d7f7345d0c0731ba76a4ecab3cdde3acd30
Kyle Harding <kyle@balena.io>
2021-06-15 12:15:38 -0400

    Prevent startup failure when hostname cannot be set
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- app/balena-hostname.sh
+++ app/balena-hostname.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# openFleets: configure hostname
-curl -X PATCH --header "Content-Type:application/json" \
-    --data "{\"network\": {\"hostname\": \"$BALENA_HOSTNAME\"}}" \
-    "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
+# set a hostname for mDNS (default to nextcloud.local)
+if [ -n "${BALENA_HOSTNAME}" ]
+then
+    curl -X PATCH --header "Content-Type:application/json" \
+        --data "{\"network\": {\"hostname\": \"${BALENA_HOSTNAME}\"}}" \
+        "${BALENA_SUPERVISOR_ADDRESS}/v1/device/host-config?apikey=${BALENA_SUPERVISOR_API_KEY}" || true
+fi
 
 exec /entrypoint.sh php-fpm
